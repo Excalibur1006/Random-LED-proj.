54e3c44a2ad8550933ecf518d2e243b413be3d2b
Knincahzuhe <mreis593@gmail.com>
2016-09-21 21:05:27 -0400

    Update Arduino Code.ino

--- Arduino Code.ino
+++ Arduino Code.ino
@@ -1,47 +1,19 @@
-int redLEDpin= 13;  //declaring red LED pin
-int redOffTime= 500;  //red LED off time
-int redOnTime= 500; //red LED on time
-int greenLEDpin= 12;  //declaring variable for green LED pin
-int greenOffTime= 500; //green LED off time
-int greenOnTime= 500;  //green LED on time
-int orngLEDpin= 11; //decalring variable for orange LED pin
-int orngOnTime=2000;  //orange LED on time
-int orngOffTime= 2000;  //orange LED off time
-int LEDvoltOn= 255;  //declaring LED on voltage
-int LEDvoltOff= 0;  //declaring LED off voltage
-long randNum;  //declaring int for random number
-
-
+int dTime = 500;
 
 void setup() {
- pinMode(OUTPUT,redLEDpin); //declaring red LED pin
- pinMode(OUTPUT,greenLEDpin); //declaring green LED pin
- pinMode(OUTPUT,orngLEDpin);  //declaring orange LED pin
- Serial.begin(9600);  //declaring baud rate of 9600
+  pinMode(1,OUTPUT); //Declare pin 1 to be an output
+  pinMode(2,OUTPUT); //Declare pin 2 to be an output
+  pinMode(3,OUTPUT); //Declare pin 3 to be an output
+
 }
 
-void loop() {
-  randNum = random(11,14);  //declaring paramiters for random number
-  Serial.println(randNum);  //prints random number to serial monitor
-  if(randNum = 13){ //if random number = 13 do this
-    analogWrite(redLEDpin,LEDvoltOn);  //applying analog voltage to red LED
-    delay(redOnTime); //applying red on time delay
-    analogWrite(redLEDpin,LEDvoltOff);  //applying analog voltage to red LED
-    delay(redOffTime);  //applying red off time delay
-  }
 
-  else if(randNum = 12) {   //If random number = 12 then do this
-    analogWrite(greenLEDpin,LEDvoltOn);  //applying analog voltage to green LED
-    delay(greenOnTime); //applying green on time delay
-    analogWrite(greenLEDpin,LEDvoltOff);  //applying analog voltage to green LED
-    delay(greenOffTime);  //applying green off time delay
-  }
+void loop(){
+  int r = rand() % 4; //Set r equivalent to the remainder of a random number divided by 4
 
-  else{ //if the above are false do this
-    analogWrite(orngLEDpin,LEDvoltOn);  //applying analog voltage to orange LED
-    delay(orngOnTime);  //applying orange on time delay
-    analogWrite(orngLEDpin,LEDvoltOff); //applying analog voltage to orange LED
-    delay(orngOffTime); //applying orange off time delay
-  }
-  delay(1000);
+  digitalWrite(r,HIGH); //Turn on a random pin
+  delay(dTime); //Wait for dTime
+  digitalWrite(r,LOW); //Turn off that same random pin
+  delay(dTime); //Wait dTime
+
 }
