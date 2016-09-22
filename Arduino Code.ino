dddcd0d024802686f6d4bb4d3e2ddd4837c61ada
Charlie Wade <colson72098@gmail.com>
2016-09-21 20:45:37 -0400

    Fixed syntax and removed unused function

--- Arduino Code.ino
+++ Arduino Code.ino
@@ -18,34 +18,30 @@ void setup() {
  pinMode(OUTPUT,greenLEDpin); //declaring green LED pin
  pinMode(OUTPUT,orngLEDpin);  //declaring orange LED pin
  Serial.begin(9600);  //declaring baud rate of 9600
- randomSeed(analogRead(0));
 }
 
 void loop() {
+  randNum = random(11,14);  //declaring paramiters for random number
+  Serial.println(randNum);  //prints random number to serial monitor
+  if(randNum = 13){ //if random number = 13 do this
+    analogWrite(redLEDpin,LEDvoltOn);  //applying analog voltage to red LED
+    delay(redOnTime); //applying red on time delay
+    analogWrite(redLEDpin,LEDvoltOff);  //applying analog voltage to red LED
+    delay(redOffTime);  //applying red off time delay
+  }
 
-randNum = random(11,13);  //declaring paramiters for random number
-Serial.println(randNum);  //prints random number to serial monitor
-delay(100); //delay
+  else if(randNum = 12) {   //If random number = 12 then do this
+    analogWrite(greenLEDpin,LEDvoltOn);  //applying analog voltage to green LED
+    delay(greenOnTime); //applying green on time delay
+    analogWrite(greenLEDpin,LEDvoltOff);  //applying analog voltage to green LED
+    delay(greenOffTime);  //applying green off time delay
+  }
 
-if(randNum = 13){ //if random number = 13 do this
-  analogWrite(redLEDpin,LEDvoltOn);  //applying analog voltage to red LED 
-  delay(redOnTime); //applying red on time delay
-  analogWrite(redLEDpin,LEDvoltOff);  //applying analog voltage to red LED
-  delay(redOffTime);  //applying red off time delay
-}
-
-else if(randNum = 12) {   //If random number = 12 then do this
-  analogWrite(greenLEDpin,LEDvoltOn);  //applying analog voltage to green LED 
-  delay(greenOnTime); //applying green on time delay
-  analogWrite(greenLEDpin,LEDvoltOff);  //applying analog voltage to green LED
-  delay(greenOffTime);  //applying green off time delay
-}  
-
-else{ //if the above are false do this
-  analogWrite(orngLEDpin,LEDvoltOn);  //applying analog voltage to orange LED 
-  delay(orngOnTime);  //applying orange on time delay
-  analogWrite(orngLEDpin,LEDvoltOff); //applying analog voltage to orange LED
-  delay(orngOffTime); //applying orange off time delay
-}
-delay(1000);
+  else{ //if the above are false do this
+    analogWrite(orngLEDpin,LEDvoltOn);  //applying analog voltage to orange LED
+    delay(orngOnTime);  //applying orange on time delay
+    analogWrite(orngLEDpin,LEDvoltOff); //applying analog voltage to orange LED
+    delay(orngOffTime); //applying orange off time delay
+  }
+  delay(1000);
 }
