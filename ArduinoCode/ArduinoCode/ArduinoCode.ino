007b5ad5c4220f3664d537e9ffceb6cb03b26190
Excalibur1006 <bruss2017@NCHS-DOM.com>
2016-09-27 09:28:07 -0400

    Finished Master

--- ArduinoCode/ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode/ArduinoCode.ino
@@ -1,19 +1,40 @@
-int dTime = 5000; //Delay time
+int redLEDpin= 13;  //declaring red LED pin
+int greenLEDpin= 12;  //declaring variable for green LED pin
+int orngLEDpin= 11; //decalring variable for orange LED pin
+int localRandNum = 0;  //declaring paramiters for random number
+int recvRandNum = 0;  //decalring variable for recieved random number
+int delayOffTime= 1000;  //red LED off time
+int delayOnTime= 1000; //red LED on time
+int x= 13;
+
+#include  <Wire.h>
 
 void setup() {
-  pinMode(1,OUTPUT); //Declare pin 1 to be an output
-  pinMode(2,OUTPUT); //Declare pin 2 to be an output
-  pinMode(3,OUTPUT); //Declare pin 3 to be an output
+
+  pinMode(redLEDpin,OUTPUT);
+  pinMode(greenLEDpin,OUTPUT);
+  pinMode(orngLEDpin,OUTPUT);
+  Serial.begin(9600);
+ 
+  Wire.begin(8); //join i2c bus (address optional for master)
 
 }
 
+void loop() {
+  localRandNum = random(11,14); 
+  Wire.beginTransmission(9);  //transmit to device #8
+  Wire.write(randNum);        //sends variable "x" to master
+  Wire.endTransmission(); //stop transmitting
+  
+                                                                                                                                                                                                                                                                                                          
+  Serial.println(randNum);  //prints random number to serial monitor
+  digitalWrite(randNum,HIGH);  //applying analog voltage to red LED
+  delay(delayOnTime); //applying red on time delay
+  digitalWrite(randNum,LOW);  //applying analog voltage to red LED
+  delay(delayOffTime);  //applying red off time delay
+    
+}  
+  
+  
+  
 
-void loop(){
-  int r = rand() % 4; //Set r equivalent to the remainder of a random number divided by 4
-
-  digitalWrite(r,HIGH); //Turn on a random pin
-  delay(dTime); //Wait for dTime
-  digitalWrite(r,LOW); //Turn off that same random pin
-  delay(dTime); //Wait dTime
-
-}
